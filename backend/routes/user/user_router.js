b348afbd88e6b2175fbc2b6e86d0c4f8d9465500
Nischaya Garg <nischayagarg008@gmail.com>
2025-01-10 21:47:03 +0530

    Update user_router.js

--- backend/routes/user/user_router.js
+++ backend/routes/user/user_router.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {getUser,follow_unfollow,getSuggested,updateUser,deleteUser} from "./user_controller.js";
+import {getUser,follow_unfollow,getSuggested,updateUser,deleteUser,getFollowing} from "./user_controller.js";
 import { sessioncheck } from "../../lib/sessioncheck.js";
 
 const router=express.Router();
 
 router.get("/profile/:name",getUser);
 router.get("/suggested",sessioncheck,getSuggested);
+router.get("/following",sessioncheck,getFollowing);
 router.post("/follow/:id",sessioncheck,follow_unfollow);
 router.post("/update",sessioncheck,updateUser);
 router.delete("/delete",sessioncheck,deleteUser);
