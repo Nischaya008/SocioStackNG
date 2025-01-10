c1989fc96e97617067e233da26a3ddbbbcb621ed
Nischaya Garg <nischayagarg008@gmail.com>
2025-01-10 21:46:42 +0530

    Update user_controller.js

--- backend/routes/user/user_controller.js
+++ backend/routes/user/user_controller.js
@@ -214,3 +214,18 @@ export const deleteUser=async(req,res)=>{
         return res.status(500).json({message:"Server error"});
     }
 };
+
+export const getFollowing = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id)
+            .populate('following', 'username profileIMG _id');
+        
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user.following);
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
