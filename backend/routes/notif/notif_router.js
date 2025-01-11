355e6cb62cb7f9866562571160bbab9277fd2a54
Nischaya Garg <nischayagarg008@gmail.com>
2025-01-11 16:51:38 +0530

    Update notif_router.js

--- backend/routes/notif/notif_router.js
+++ backend/routes/notif/notif_router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {sessioncheck} from "../../lib/sessioncheck.js";
-import { getNotifs, delNotifs, delNotif } from "./notif_controller.js";
+import { getNotifs, delNotifs, delNotif, getUnreadCount } from "./notif_controller.js";
 
 const router=express.Router();
 
 router.get("/",sessioncheck, getNotifs);
+router.get("/unread",sessioncheck, getUnreadCount);
 router.delete("/",sessioncheck, delNotifs);
 router.delete("/:id",sessioncheck, delNotif);
 export default router;
